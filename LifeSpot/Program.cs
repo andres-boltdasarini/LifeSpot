1a46b2a88278e0360387912f4b9e1fcf326a2687
Андрей <lau47@mail.ru>
2025-09-02 09:57:45 +0300

    Добавьте файлы проекта.

---
+++ LifeSpot/Program.cs
@@ -0,0 +1,18 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseStaticFiles();
+
+app.MapGet("/", async context =>
+{
+    var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "index.html");
+    var html = await File.ReadAllTextAsync(viewPath);
+    await context.Response.WriteAsync(html);
+});
+
+app.Run();
