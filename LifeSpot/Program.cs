8e0f45ce2b49c6eac0882cc685291fb5c0ab3a6d
Андрей <lau47@mail.ru>
2025-09-16 21:54:04 +0300

    2724

--- LifeSpot/Program.cs
+++ LifeSpot/Program.cs
@@ -15,11 +15,19 @@ app.UseStaticFiles(new StaticFileOptions
     RequestPath = "/Static"
 });
 
+
 app.MapGet("/", async context =>
 {
     var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "index.html");
     var html = await File.ReadAllTextAsync(viewPath);
     await context.Response.WriteAsync(html);
+});
+app.MapGet("/Static/CSS/index.css", async context =>
+{
+    // по аналогии со страницей Index настроим на нашем сервере путь до страницы со стилями, чтобы браузер знал, откуда их загружать
+    var cssPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "CSS", "index.css");
+    var css = await File.ReadAllTextAsync(cssPath);
+    await context.Response.WriteAsync(css);
 });
 
 app.Run();
