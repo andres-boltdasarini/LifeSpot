23a4fd048ef40fd85d810498e7a949605e745795
akupriyanov <akupriyanov@astralinux.ru>
2025-09-24 14:33:23 +0300

    2911

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,4 +1,24 @@
-﻿// создадим объект Map для хранения сессии
+﻿function filterContent(){
+    let inputString = document.getElementsByTagName('input')[0].value.toLowerCase();
+    // Находим контейнеры с видео, которые необходимо фильтровать
+    let elements = document.getElementsByClassName('video-container');
+     
+    // Пробегаемся по контейнерам
+    for (let i = 0; i < elements.length; i++ ){
+       // Вытаскиваем текст описания видео, которое необходимо отфильтровать
+       let videoText = elements[i].querySelector('.video-title').innerText;
+       // Выполняем фильтрацию, сравнивая значения в нижнем регистре
+       if(!videoText.toLowerCase().includes(inputString.toLowerCase())){
+           // Описание
+           elements[i].style.display = 'none';
+       } else {
+           elements[i].style.display = 'inline-block';
+       }
+    }
+
+ }
+
+// создадим объект Map для хранения сессии
 let session =  new Map();
 // Сохраним UserAgent
 session.set("userAgent", window.navigator.userAgent)
