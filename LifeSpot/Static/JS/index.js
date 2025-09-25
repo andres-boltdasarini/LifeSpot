b4848d8fefba018e132140374ec6afa17380a6e3
Андрей <lau47@mail.ru>
2025-09-25 22:42:10 +0300

    2938

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,10 +1,16 @@
-﻿let contentFilter =  function filterContent(){
-    let inputString = document.getElementsByTagName('input')[0].value.toLowerCase();
+﻿// Логирование сессии (объявлено через expression)
+let sessionLog = function logSession(session) {
+    // Вывод в консоль
+    for (let result of session) {
+        console.log(result)
+    }
+}
+function filterContent(userInput) { 
     let elements = document.getElementsByClassName('video-container');
   
     for (let i = 0; i <= elements.length; i++ ){
         let videoText = elements[i].querySelector(".video-title").innerText;
-        if(!videoText.toLowerCase().includes(inputString.toLowerCase())){
+        if (!videoText.toLowerCase().includes(userInput.toLowerCase())){
             elements[i].style.display = 'none';
         } else {
             elements[i].style.display = 'inline-block';
@@ -12,7 +18,7 @@
     }
  }
 
- let sessionHandler = function handleSession(){
+function handleSession() {
     let session =  new Map();
     session.set("userAgent", window.navigator.userAgent)
     session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
@@ -26,8 +32,7 @@
         alert("Наши трансляции не предназначены для лиц моложе 18 лет. ВыL будете перенаправлены");
         window.location.href = "http://www.google.com"
     }
-    for (let result of session){
-        console.log(result)
+    return session;
     }
- }
+ 
   
