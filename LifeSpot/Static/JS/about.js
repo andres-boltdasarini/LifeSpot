5674df6a1e18a8d4ead55d56301ffc9ed0b40693
Андрей <lau47@mail.ru>
2025-09-30 16:23:07 +0300

    297

--- LifeSpot/Static/JS/about.js
+++ LifeSpot/Static/JS/about.js
@@ -1,5 +1,35 @@
 // Получим коллекцию всех элементов страницы
-let elements = document.getElementsByTagName('*');
- 
+//let elements = document.getElementsByTagName('*');
+
 // Выведем результат в уведомление
-alert(`Количество элементов на странице:  ${elements.length}`);
+//alert(`Количество элементов на странице:  ${elements.length}`);
+
+function getReview() {
+    // Создадим объект
+    let review = {}
+
+    // Сохраним свойство имени
+    review["userName"] = prompt("Как вас зовут ?")
+    if (review["userName"] == null) {
+        return
+    }
+
+    // Сохраним текст отзыва
+    review["comment"] = prompt("Напишите свой отзыв")
+    if (review["comment"] == null) {
+        return
+    }
+
+    // Сохраним текущее время
+    review["date"] = new Date().toLocaleString()
+
+    // Добавим на страницу
+    writeReview(review)
+}
+
+const writeReview = review => {
+    document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
+        `<p> <i> <b>${review['userName']}</b>  ${review['date']}</i></p>` +
+        `<p>${review['comment']}</p>` +
+        '</div>';
+}
