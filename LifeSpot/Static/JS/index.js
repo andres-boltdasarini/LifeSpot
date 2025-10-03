0465bc261eed536417189eabe1d407dc7ff12042
akupriyanov <akupriyanov@astralinux.ru>
2025-10-03 12:13:21 +0300

    30112

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -3,19 +3,55 @@
 * Будет "захватываться" тремя функциями
 * 
 * */ 
-let session =  {
-    'startDate' : new Date().toLocaleString(),
-    'userAgent' : window.navigator.userAgent,
-    'userAge' : prompt("Пожалуйста, введите ваш возраст?")
+// let session =  {
+//     'startDate' : new Date().toLocaleString(),
+//     'userAgent' : window.navigator.userAgent,
+//     'userAge' : prompt("Пожалуйста, введите ваш возраст?")
+//  }
+
+ function handleSession (logger, checker){
+  
+    // Проверяем дату захода и проставляем, если новый визит
+    if(window.sessionStorage.getItem("startDate") == null){
+        window.sessionStorage.setItem("startDate", new Date().toLocaleString())
+    }
+  
+    // Проверяем userAgent и проставляем, если новый визит
+    if(window.sessionStorage.getItem("userAgent") == null){
+        window.sessionStorage.setItem("userAgent", window.navigator.userAgent)
+    }
+  
+    // Проверяем возраст и проставляем, если новый визит
+    if(window.sessionStorage.getItem("userAge") == null){
+        let input = prompt("Пожалуйста, введите ваш возраст?");
+        window.sessionStorage.setItem("userAge", input)
+       
+        /* Возраст отсутствовал в sessionStorage. Значит, это первый визит пользователя, и
+         при прохождении проверки на возраст он увидит приветствие*/
+        checker(true)
+    }else{
+  
+        /* Пользователь заходит не первый раз, приветствие не показываем. */
+        checker(false)
+    }
+   
+    /* Вызываем переданную в качестве колл-бэка функцию логирования.
+        передавать в качестве коллбека не обязательно, можно вызвать и напрямую, но мы добавили для повторения.
+    */
+    logger()
  }
 
 /*
 * Проверка возраста пользователя
 * 
 * */
-function checkAge(){
-    if(session.userAge >= 18){
-        alert("Приветствуем на LifeSpot! " + '\n' +  "Текущее время: " + new Date().toLocaleString() );
+ let checker = function ( newVisit ){
+    if(window.sessionStorage.getItem("userAge") >= 18 ){
+        // Добавим проверку на первое посещение, чтобы не показывать приветствие
+        // лишний раз
+        if(newVisit){
+            alert("Приветствуем на LifeSpot! " + '\n' +  "Текущее время: " + new Date().toLocaleString() );
+        }
     }
     else{
         alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
@@ -24,15 +60,14 @@ function checkAge(){
  }
 
 
-
 /*
 * Вывод данных сессии в консоль
 * 
 * */
-let sessionLog = function () {
-    console.log('Начало сессии: ' + session.startDate)
-    console.log('Даныне клиента: ' + session.userAgent)
-    console.log('Возраст пользователя: : ' + session.userAge)
+ let logger = function () {
+    console.log('Начало сессии: ' + window.sessionStorage.getItem("startDate") )
+    console.log('Даныне клиента: ' + window.sessionStorage.getItem("userAgent") )
+    console.log('Возраст пользователя: ' + window.sessionStorage.getItem("userAge"))
  }
 /*
 * Функция для фильтраци контента
