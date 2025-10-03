abe5aca5e56fa344178ea6e4b610f97f43052f97
akupriyanov <akupriyanov@astralinux.ru>
2025-10-03 17:29:44 +0300

    3024

--- LifeSpot/Static/JS/about.js
+++ LifeSpot/Static/JS/about.js
@@ -3,36 +3,58 @@
 * и сохраним отзыв в объект
 * 
 * */
-function getReview() {
-    // Создадим объект
-    let review = {}
-    
-    // Сохраним свойство имени
-    review["userName"] = prompt("Как вас зовут ?")
-    if(review["userName"] == null){
-        return
-    }
-    
-    // Сохраним текст отзыва
-    review["comment"] = prompt("Напишите свой отзыв")
-    if(review["comment"] == null){
-        return
-    }
-    
-    // Сохраним текущее время
-    review["date"] = new Date().toLocaleString()
-    
-    // Добавим на страницу
-    writeReview(review)
-}
 
 /*
 * Запишем отзыв на страницу 
 * 
 * */
 const writeReview = review => {
+    let likeCounter = '';
+   
+    // Для проверки, является ли объект отзывом, используем свойство hasOwnProperty
+    if(review.hasOwnProperty('rate')){
+        likeCounter += '           <b style="color: chocolate">Рейтинг:</b>   ' + review.rate;
+    }
+   
+    // Запишем результат
     document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
-        `<p> <i> <b>${review['userName']}</b>  ${review['date']}</i></p>` +
-        `<p>${review['comment']}</p>`  +
+        `<p> <i> <b>${review['author']}</b>  ${review['date']}${likeCounter}</i></p>` +
+        `<p>${review['text']}</p>`  +
         '</div>';
-}
+ }
+
+function getComment() {
+    // Создаем объект обычного комментария
+    let comment = {}
+   
+    // Запросим имя
+    comment.author = prompt("Как вас зовут ?")
+    if(comment.author== null){
+        return
+    }
+   
+    // Запросим текст
+    comment.text = prompt("Оставьте отзыв")
+    if(comment.text == null){
+        return
+    }
+  
+    // Сохраним текущее время
+    comment.date = new Date().toLocaleString()
+   
+    // Запросим, хочет ли пользователь оставить полноценный отзыв или это будет обычный комментарий
+    let enableLikes = confirm('Разрешить пользователям оценивать ваш отзыв?')
+   
+    if(enableLikes){
+        // Создадим для отзыва новый объект из прототипа - комментария
+        let review = Object.create(comment)
+        // и добавим ему нужное свойство
+        review.rate = 0;
+  
+        // Добавляем отзыв с возможностью пользовательских оценок
+        writeReview(review)
+    } else{
+        // Добавим простой комментарий без возможности оценки
+        writeReview(comment)
+    }
+ }
