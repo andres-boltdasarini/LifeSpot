6652ce194b7f68dd6e9cb843b355ed78f9408a17
akupriyanov <akupriyanov@astralinux.ru>
2025-10-03 11:33:07 +0300

    30111

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,16 +1,52 @@
-﻿// Логирование сессии (объявлено через expression)
-let sessionLog = function logSession() {
-    for (let result of session) {
-        console.log(result)
-    }
-}
-function filterContent() { // в этот параметр будет передана ФУНКЦИЯ парсинга пользовательского ввода
+﻿/*
+* Сессия теперь создается в общей области видимости.
+* Будет "захватываться" тремя функциями
+* 
+* */ 
+let session =  {
+    'startDate' : new Date().toLocaleString(),
+    'userAgent' : window.navigator.userAgent,
+    'userAge' : prompt("Пожалуйста, введите ваш возраст?")
+ }
 
+/*
+* Проверка возраста пользователя
+* 
+* */
+function checkAge(){
+    if(session.userAge >= 18){
+        alert("Приветствуем на LifeSpot! " + '\n' +  "Текущее время: " + new Date().toLocaleString() );
+    }
+    else{
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+ }
+
+
+
+/*
+* Вывод данных сессии в консоль
+* 
+* */
+let sessionLog = function () {
+    console.log('Начало сессии: ' + session.startDate)
+    console.log('Даныне клиента: ' + session.userAgent)
+    console.log('Возраст пользователя: : ' + session.userAge)
+ }
+/*
+* Функция для фильтраци контента
+* Будет вызываться благодаря атрибуту oninput на index.html
+* 
+* */
+
+function filterContent(){
     let elements = document.getElementsByClassName('video-container');
 
-    for (let i = 0; i <= elements.length; i++) {
-        let videoText = elements[i].querySelector(".video-title").innerText;
-        if (!videoText.toLowerCase().includes(inputParseFunction() /*Переданная функция вызвана*/.toLowerCase())) {
+    for (let i = 0; i <= elements.length; i++ ){
+        let videoText = elements[i].getElementsByTagName('h3')[0].innerText;
+
+        if(!videoText.toLowerCase().includes(inputParseFunction().toLowerCase())){
             elements[i].style.display = 'none';
         } else {
             elements[i].style.display = 'inline-block';
@@ -18,24 +54,14 @@ function filterContent() { // в этот параметр будет перед
     }
 }
 
+/*
+* Всплывающее окно будет показано по таймауту
+* 
+* */
+// setTimeout(() =>
+//     alert("Нравится LifeSpot? " + '\n' +  "Подпишитесь на наш Instagram @lifespot999!" ),
+// 7000);
 
-let session = new Map();
-function handleSession() {
-    // Сохраним время начала сессии
-    session.set("startDate", new Date().toLocaleString())
-    // Сохраним UserAgent
-    session.set("userAgent", window.navigator.userAgent)
-}
 
-function checkAge() {
-    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
 
-    if (session.get("age") >= 18) {
-        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
-    }
-    else {
-        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
-        window.location.href = "http://www.google.com"
-    }
-}
-  
+
--- LifeSpot/Static/JS/testing.js
+++ LifeSpot/Static/JS/testing.js
@@ -1,5 +1,18 @@
-// Получим коллекцию всех элементов страницы
-let elements = document.getElementsByTagName('*');
- 
-// Выведем результат в уведомление
-alert(`Количество элементов на странице:  ${elements.length}`);
+﻿// Создаем объект Map
+let myMap = new Map();
+myMap.set("Германия", "Берлин")
+myMap.set("Швеция", "Стокгольм")
+myMap.set(1, "Москва")
+
+let newArray = [];
+// Перебор Map в цикле for
+for (let result of myMap){
+    newArray.push(result);
+}
+console.log(newArray);
+
+let newArrayOfStrings = [];
+// Перебор Map с помощью Array.from
+// Позволяет на лету выполнять операции с парой ключ-значение
+Array.from(myMap, ([key,value]) => newArrayOfStrings.push(`${key} - ${value}`) );
+console.log(newArrayOfStrings);
