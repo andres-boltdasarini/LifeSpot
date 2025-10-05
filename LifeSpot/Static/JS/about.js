a46df270483fef9ce270f4fd2b0545e8a1d75744
Андрей <lau47@mail.ru>
2025-10-05 15:23:16 +0300

    3037

--- LifeSpot/Static/JS/about.js
+++ LifeSpot/Static/JS/about.js
@@ -10,18 +10,19 @@
 * */
 const writeReview = review => {
     let likeCounter = '';
-   
-    // Для проверки, является ли объект отзывом, используем свойство hasOwnProperty
-    if(review.hasOwnProperty('rate')){
-        likeCounter += '           <b style="color: chocolate">Рейтинг:</b>   ' + review.rate;
+
+    // Если публикуется отзыв - добавляем ему кнопку с лайками.
+    if (review.hasOwnProperty('rate')) {
+
+        // Генерим идентификатор комментария.
+        let commentId = Math.random();
+        // Для кнопки лайков добавляем: идентификатор, атрибут onclick для передачи идентификатора в функцию, значок лайка, и само значение счётчика отделяем пробелом
+        // Также мы добавили стиль, чтобы кнопка смотрелась лучше и не имела рамок
+        likeCounter += '<button id="' + commentId + '" style="border: none" onclick="addLike(this.id)">' + `❤️ ${review.rate}</button>`
     }
-   
-    // Запишем результат
-    document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
-        `<p> <i> <b>${review['author']}</b>  ${review['date']}${likeCounter}</i></p>` +
-        `<p>${review['text']}</p>`  +
-        '</div>';
- }
+    // Запишем результат 
+    document.getElementsByClassName('reviews')[0].innerHTML += ' <div class="review-    text">\n' + `<p> <i> <b>${review['author']}</b> ${review['date']}${likeCounter}</i></p>` + `<p>${review['text']}</p>` + '</div>';
+}
 
 function Comment() {
     // Создаем объект обычного комментария
@@ -68,4 +69,25 @@ function addComment() {
         // Добавим простой комментарий без возможности оценки
         writeReview(comment)
     }
- }
+}
+
+function addLike(id) {
+    // Найдём нужный элемент по id
+    let element = document.getElementById(id);
+
+    // Преобразуем текст элемента в массив, разбив его по пробелам (так как счётчик лайков у нас отделен от символа ❤️пробелом)
+    let array = element.innerText.split(' ')
+
+    // Вытащим искомое значение счётчика и сразу же преобразуем его в число, так как
+    // при сложении любого значения со строкой в JS будет строка, а нам этого не требуется
+    let resultNum = parseInt(array[array.length - 1], 10);
+
+    // Увеличим счётчик
+    resultNum += 1
+
+    // Сохраним измененное значение обратно в массив
+    array[array.length - 1] = `${resultNum}`
+
+    // Обновим текст элемента
+    element.innerText = array.join(' ')
+}
